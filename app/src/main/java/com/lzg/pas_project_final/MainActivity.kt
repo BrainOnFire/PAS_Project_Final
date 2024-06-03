a748e4d4947bd3bb6bb5aceb8409a60eef9b0374
Luciano Zagastizabal <67831938+BrainOnFire@users.noreply.github.com>
2024-06-04 00:19:00 +0200

    Initial commit

---
+++ app/src/main/java/com/lzg/pas_project_final/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.lzg.pas_project_final
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.lzg.pas_project_final.ui.theme.PAS_Project_FinalTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PAS_Project_FinalTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PAS_Project_FinalTheme {
+        Greeting("Android")
+    }
+}
