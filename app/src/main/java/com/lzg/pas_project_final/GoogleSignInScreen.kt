f67228cc892ee68e9fdfb972147dba2b0b36090d
Luciano Zagastizabal <lucianozg98@gmail.com>
2024-06-06 18:12:00 +0200

    SignIn Corregido

--- app/src/main/java/com/lzg/pas_project_final/GoogleSignInScreen.kt
+++ app/src/main/java/com/lzg/pas_project_final/GoogleSignInScreen.kt
@@ -1,20 +1,30 @@
 package com.lzg.pas_project_final
 
-import android.text.Layout
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 @Composable
 fun SignInScreen() {
-    var email by remember { mutableStateOf("") }
+    var email by remember {mutableStateOf("")}
     var password by remember { mutableStateOf("") }
     var showError by remember { mutableStateOf(false) }
     val auth = Firebase.auth
@@ -22,20 +32,25 @@ fun SignInScreen() {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Layout.Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         OutlinedTextField(
-            value = email,
+            value = email ,
             onValueChange = { email = it },
-            label = { Text("Email") },
-            modifier = Modifier.fillMaxWidth().padding(16.dp)
+            label = { Text(text = "Email") },
+            //leadingIcon = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
         )
         OutlinedTextField(
             value = password,
             onValueChange = { password = it },
-            label = { Text("Password") },
+            label = { Text(text = "Password") },
             visualTransformation = PasswordVisualTransformation(),
-            modifier = Modifier.fillMaxWidth().padding(16.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
         )
         if (showError) {
             Text(
@@ -63,7 +78,7 @@ fun SignInScreen() {
             },
             modifier = Modifier.padding(16.dp)
         ) {
-            Text("Sign In")
+            Text(text = "Sign In")
         }
     }
 }
